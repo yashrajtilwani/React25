454aa0b6be84a4f9767e05aa536129b8459c2ba3
Yashraj <yashrajtilwani73099@gmail.com>
2025-03-10 17:55:55 +0530

    2. Random Color Generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import Accordian from './Components.jsx/Accordian/Accordian'
 import RandomColor from './Components.jsx/RandomColor/RandomColor'
+import './App.css'
 
 function App() {
   
 
   return (
-    <>
+    <div className='App'>
       <Accordian />
-    </>
+      <RandomColor />
+    </div>
   )
 }
 
--- src/Components.jsx/RandomColor/RandomColor.jsx
+++ src/Components.jsx/RandomColor/RandomColor.jsx
@@ -1,9 +1,42 @@
+import { useState } from "react";
 import "./RandomColor.css"
 
 export default function RandomColor(){
+    let [color, setColor] = useState(updateHex);
+
+    //type === true => Hex else RGB
+    let [type, setType] = useState(true);
+    
+    function randomGenerator(nums){
+        return (Math.floor(Math.random() * nums));
+    }
+
+    function updateRgb(){
+        let color = `rgb(${randomGenerator(256)},${randomGenerator(256)},${randomGenerator(256)})`;
+        setColor(color);
+    }
+
+    function updateHex(){
+        let color = "#";
+        let options = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+        for(let i = 0; i< 6; i+=1){
+            color += `${options[randomGenerator(16)]}` 
+        }
+        return color;
+    }
+
     return (
-        <div className="RandomColor">
-            <h1>Random Color Generator</h1>
+        <div className="RandomColorContainer" style={{backgroundColor: color}}>
+            <div className="RandomColor">
+                <h1 className="ColorHeading">Random Color Generator</h1>
+                <h2 className="ColorHeading">{color}</h2>
+                <div className="type">
+                    <button className="typeButton" onClick={() => setType(true)}>HEX Color</button>
+                    <button className="typeButton" onClick={() => setType(false)}>RGB Color</button>
+                </div>
+                <button className="typeButton" onClick={type ? () => setColor(updateHex) : updateRgb}>Random Color</button>
+            </div>
+            
         </div>
     )
 }
